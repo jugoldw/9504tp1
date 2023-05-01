67c37762ea18b969108bf80534a64e944478b110
jugoldw <132274508+jugoldw@users.noreply.github.com>
2023-05-01 18:22:33 -0300

    Update raices_biseccion.py

--- raices_biseccion.py
+++ raices_biseccion.py
@@ -9,34 +9,38 @@ def f2(x):
 def f3(x):
     return (x - 0.9) * np.exp(-4 * (x - 0.9)**2)
         
-def biseccion(f,a,b,tol):
-    x0 = (a+b) / 2
-    x1 = (x0 + a) / 2 if np.sign(f(x0)) != np.sign(f(a)) else (x0 + b) / 2
+def biseccion(f,a,b,tol,n):
+    
+    if f(a) * f(b) >= 0: # No puede ser así (hipotesis del teorema de Bolsano)
+        print("No puede existir una raiz en el intervalo dado.")
+        return None
+    
+    x0 = a
 
-    while np.abs(x1 - x0) > tol:
-
-        if np.sign(f(x1)) != np.sign(f(x0)):
-            a = np.min([x0,x1])
-            b = np.max([x0,x1])       
-
+    i = 1
+    while i < n:
+        x1 = (a+b) / 2 
+    
+        if np.abs(x1 - x0) < tol:
+            return x1
+        
+        if np.sign(f(x1)) == np.sign(f(a)):
+            a = x1
         else:
-            if x1 < x0:
-                b = x1
-
-            else:
-                a = x1
+            b = x1    
 
         x0 = x1
-        x1 = (a+b) / 2
-
-    return x1
+        i = i + 1
+    print("Se ha superado la máxima cantidad de iteraciones posibles.")
+    return None
 
 F = [f1,f2,f3]
+maxIter = 500 # Cant maxima de iteraciones, por si la tolerancia dada es demasiado chica y nunca se alcanza
 
 print('Tolerancia = 10^-5')
 for i in range(0,3):
-    print('Raiz ' + str(i+1) + ': ' + str(biseccion(F[i],0,3,1e-5)))
+    print('Raiz ' + str(i+1) + ': ' + str(biseccion(F[i],0,3,1e-5,maxIter)))
 
 print('Tolerancia = 10^-13')
 for i in range(0,3):
-    print('Raiz ' + str(i+1) + ': ' + str(biseccion(F[i],0,3,1e-13)))
+    print('Raiz ' + str(i+1) + ': ' + str(biseccion(F[i],0,3,1e-13,maxIter)))
